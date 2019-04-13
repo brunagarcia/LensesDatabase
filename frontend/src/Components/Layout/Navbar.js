c018a007c487fab2b09fe41e2fd143ccb0db96f8
Bruna <garciabrunap@gmail.com>
2019-04-13 14:59:18 -0700

    Starting with Material UI

--- frontend/src/Components/Layout/Navbar.js
+++ frontend/src/Components/Layout/Navbar.js
@@ -3,13 +3,14 @@ import React from 'react';
 // import SignOutLinks from './SignOutLinks';s
 import SignInLinks from './SignInLinks';
 import { Link } from 'react-router-dom';
-
+import { AppBar } from '@material-ui/core'
 
 const Navbar = () => {
   return (
     <div>
       <nav className="nav-wrapper grey darken-3">
         <div className="container">
+          <AppBar>Material ui core</AppBar>
           <Link to='/' className="brand-logo">Lens-yclopedia</Link>
           <SignInLinks></SignInLinks>
           {/* <SignOutLinks></SignOutLinks> */}
